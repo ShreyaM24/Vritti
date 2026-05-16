ec6ffaa5e76bd10ddf1c5f5800a738cdc2045df7
Shreya Mukherjee <shreyamukherjee1717@gmail.com>
2026-05-16 21:48:16 +0530

    Update CORS settings to allow additional origin

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,11 @@ const path = require('path');
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
+  origin: [
+    "http://localhost:5173",
+    "https://vritti-1.onrender.com"
+  ],
+  credentials: true
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
